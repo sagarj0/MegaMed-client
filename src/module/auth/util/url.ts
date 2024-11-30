9258c1363cd75df7bd6502991efc717880a1012b
sagarj0 <joshhisagar9375@gmail.com>
2024-11-30 19:17:04 +0545

    changes

--- src/module/auth/util/url.ts
+++ src/module/auth/util/url.ts
@@ -1,6 +1,6 @@
 export const AuthUrl = {
   login: "/auth/login/",
-  authSuccess: "/auth/success/",
+  authSuccess: "/auth/success",
   signUp: "/auth/signup/",
   forgotPassword: "/auth/forgot-password/",
   resetPassword: "/auth/reset-password/",
