1fa38705e56f51675b3f91cabcead31c864ca069
sagarj0 <joshhisagar9375@gmail.com>
2024-12-09 02:39:53 +0545

    mentor reducers updated

--- src/module/admin/ui/admins/add/index.tsx
+++ src/module/admin/ui/admins/add/index.tsx
@@ -6,14 +6,15 @@ import { customRequiredMark } from "@/helper/form/custom-required-mark";
 import { FormLayout } from "@/helper/form/form-layout";
 import { useAppDispatch, useAppSelector } from "@/store/hook";
 import useStatusMessage from "@/helper/hooks/use-message";
-import { resetError, resetSuccess } from "@/module/admin/service/Questions/add/reducer";
-import { resetError as resetEditError, resetSuccess as resetEditSuccess } from "@/module/admin/service/Questions/edit/reducer";
-import { useParams } from "react-router-dom";
+import { resetError, resetSuccess } from "@/module/admin/service/Users/Admins/add/reducer";
+import { resetError as resetEditError, resetSuccess as resetEditSuccess } from "@/module/admin/service/Users/Admins/edit/reducer";
+import { useNavigate, useParams } from "react-router-dom";
 import { addAdmin } from "@/module/admin/service/Users/Admins/add/action";
 import { editAdminAction } from "@/module/admin/service/Users/Admins/edit/action";
 import { mapToForm } from "./helper";
 import { BasicSection } from "./basic-section";
 import useFetchUser from "@/module/admin/hooks/useFetchUser";
+import { AdminUrls } from "@/module/admin/util/urls";
 
 interface Props {
   mode: "New" | "Edit";
@@ -23,6 +24,7 @@ export const AddAdmin: React.FC<Props> = ({ mode }) => {
   const [form] = Form.useForm<AddAdminProps>();
   const dispatch = useAppDispatch();
   const title = "Admin";
+  const navigate = useNavigate();
 
   const { id } = useParams();
   const { data: editData } = useFetchUser(id);
@@ -36,11 +38,11 @@ export const AddAdmin: React.FC<Props> = ({ mode }) => {
     if (mode === "Edit" && id) dispatch(editAdminAction({ oldData: mapToForm(editData), newData: values }));
   };
 
-  const { success, error, isLoading } = useAppSelector((root) => root.AddQuestion);
-  useStatusMessage({ success, error, resetError, resetSuccess });
-
-  const { success: successEdit, error: errorEdit } = useAppSelector((root) => root.EditQuestion);
-  useStatusMessage({ success: successEdit, error: errorEdit, resetError: resetEditError, resetSuccess: resetEditSuccess });
+  const onSuccessReset = () => navigate(AdminUrls.adminAdmin.viewAll);
+  const { success, error, isLoading } = useAppSelector((root) => root.AddAdmin);
+  useStatusMessage({ success, error, resetError, resetSuccess, onSuccessReset });
+  const { success: successEdit, error: errorEdit } = useAppSelector((root) => root.EditAdmin);
+  useStatusMessage({ success: successEdit, error: errorEdit, resetError: resetEditError, resetSuccess: resetEditSuccess, onSuccessReset });
 
   return (
     <FormLayout
